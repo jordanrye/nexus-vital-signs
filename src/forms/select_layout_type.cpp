4af0cfa8d699f6a03253e414831c52efaba8c0d6
Jordan Rye <jordanrye1@gmail.com>
2026-03-15 17:14:14 +1100

    Disabled Radial layouts until indicators have been properly implemented.

--- src/forms/select_layout_type.cpp
+++ src/forms/select_layout_type.cpp
@@ -4,12 +4,12 @@ void form_SelectLayoutType(std::string& layoutType, bool isDisabled)
 {
     static const char* layoutTypeOptions[] {
         "Grid",
-        "Radial"
+        // "Radial"
     };
 
     int option = 0; // Default to "Grid"
     if (layoutType == "Grid") option = 0;
-    else if (layoutType == "Radial") option = 1;
+    // else if (layoutType == "Radial") option = 1;
 
     if (isDisabled) { ImGui::BeginDisabled(); }
     ImGui::Combo("Type##", &option, layoutTypeOptions, IM_ARRAYSIZE(layoutTypeOptions));
