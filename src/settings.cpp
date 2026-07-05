611782ad1fbc654b39b8cddc3bc30f091eb69328
Jordan Rye <jordanrye1@gmail.com>
2026-07-05 22:51:48 +1000

    Fixed saving for "Profession" and "Heat Map" colour palettes.

--- src/settings.cpp
+++ src/settings.cpp
@@ -36,15 +36,47 @@ namespace Settings
                 {
                     auto& colors = settings["colors"];
 
-                    /* Simple Colours */
-                    dser_ImColor(colors["background"], ColourPresets.COLOUR_BACKGROUND);
-                    dser_ImColor(colors["health"], ColourPresets.COLOUR_HEALTH);
-                    dser_ImColor(colors["health-downed"], ColourPresets.COLOUR_HEALTH_DOWNED);
-                    dser_ImColor(colors["health-defeated"], ColourPresets.COLOUR_HEALTH_DEFEATED);
-                    dser_ImColor(colors["health-shroud-necromancer"], ColourPresets.COLOUR_SHROUD_NECROMANCER);
-                    dser_ImColor(colors["health-shroud-specter"], ColourPresets.COLOUR_SHROUD_SPECTER);
-                    dser_ImColor(colors["barrier"], ColourPresets.COLOUR_BARRIER);
-                    dser_ImColor(colors["hovered"], ColourPresets.COLOUR_HOVERED);
+                    /* Default Colours */
+                    dser_ImColor(colors["default-background"], ColourPresets.COLOUR_BACKGROUND);
+                    dser_ImColor(colors["default-health"], ColourPresets.COLOUR_HEALTH);
+                    dser_ImColor(colors["default-health-downed"], ColourPresets.COLOUR_HEALTH_DOWNED);
+                    dser_ImColor(colors["default-health-defeated"], ColourPresets.COLOUR_HEALTH_DEFEATED);
+                    dser_ImColor(colors["default-health-shroud-necromancer"], ColourPresets.COLOUR_SHROUD_NECROMANCER);
+                    dser_ImColor(colors["default-health-shroud-specter"], ColourPresets.COLOUR_SHROUD_SPECTER);
+                    dser_ImColor(colors["default-barrier"], ColourPresets.COLOUR_BARRIER);
+                    dser_ImColor(colors["default-hovered"], ColourPresets.COLOUR_HOVERED);
+
+                    /* Profession Colours */
+                    dser_ImColor(colors["profession-background"], ColourPresets.COLOUR_PROF_BACKGROUND);
+                    dser_ImColor(colors["profession-health-elementalist"], ColourPresets.COLOUR_PROF_HEALTH_ELEMENTALIST);
+                    dser_ImColor(colors["profession-health-engineer"], ColourPresets.COLOUR_PROF_HEALTH_ENGINEER);
+                    dser_ImColor(colors["profession-health-guardian"], ColourPresets.COLOUR_PROF_HEALTH_GUARDIAN);
+                    dser_ImColor(colors["profession-health-mesmer"], ColourPresets.COLOUR_PROF_HEALTH_MESMER);
+                    dser_ImColor(colors["profession-health-necromancer"], ColourPresets.COLOUR_PROF_HEALTH_NECROMANCER);
+                    dser_ImColor(colors["profession-health-ranger"], ColourPresets.COLOUR_PROF_HEALTH_RANGER);
+                    dser_ImColor(colors["profession-health-revenant"], ColourPresets.COLOUR_PROF_HEALTH_REVENANT);
+                    dser_ImColor(colors["profession-health-thief"], ColourPresets.COLOUR_PROF_HEALTH_THIEF);
+                    dser_ImColor(colors["profession-health-warrior"], ColourPresets.COLOUR_PROF_HEALTH_WARRIOR);
+                    dser_ImColor(colors["profession-health-downed"], ColourPresets.COLOUR_PROF_HEALTH_DOWNED);
+                    dser_ImColor(colors["profession-health-defeated"], ColourPresets.COLOUR_PROF_HEALTH_DEFEATED);
+                    dser_ImColor(colors["profession-health-shroud-necromancer"], ColourPresets.COLOUR_PROF_SHROUD_NECROMANCER);
+                    dser_ImColor(colors["profession-health-shroud-specter"], ColourPresets.COLOUR_PROF_SHROUD_SPECTER);
+                    dser_ImColor(colors["profession-barrier"], ColourPresets.COLOUR_PROF_BARRIER);
+                    dser_ImColor(colors["profession-hovered"], ColourPresets.COLOUR_PROF_HOVERED);
+                    
+                    /* Heat Map Colours */
+                    dser_ImColor(colors["heat-map-background"], ColourPresets.COLOUR_HEATMAP_BACKGROUND);
+                    dser_ImColor(colors["heat-map-health-100"], ColourPresets.COLOUR_HEATMAP_HEALTH_100);
+                    dser_ImColor(colors["heat-map-health-75"], ColourPresets.COLOUR_HEATMAP_HEALTH_75);
+                    dser_ImColor(colors["heat-map-health-50"], ColourPresets.COLOUR_HEATMAP_HEALTH_50);
+                    dser_ImColor(colors["heat-map-health-25"], ColourPresets.COLOUR_HEATMAP_HEALTH_25);
+                    dser_ImColor(colors["heat-map-health-0"], ColourPresets.COLOUR_HEATMAP_HEALTH_0);
+                    dser_ImColor(colors["heat-map-health-downed"], ColourPresets.COLOUR_HEATMAP_HEALTH_DOWNED);
+                    dser_ImColor(colors["heat-map-health-defeated"], ColourPresets.COLOUR_HEATMAP_HEALTH_DEFEATED);
+                    dser_ImColor(colors["heat-map-health-shroud-necromancer"], ColourPresets.COLOUR_HEATMAP_SHROUD_NECROMANCER);
+                    dser_ImColor(colors["heat-map-health-shroud-specter"], ColourPresets.COLOUR_HEATMAP_SHROUD_SPECTER);
+                    dser_ImColor(colors["heat-map-barrier"], ColourPresets.COLOUR_HEATMAP_BARRIER);
+                    dser_ImColor(colors["heat-map-hovered"], ColourPresets.COLOUR_HEATMAP_HOVERED);
 
                     /** TODO: Move into separate `Border` config. */
                     dser_ImColor(colors["border"], BorderPresets.COLOUR_BORDER);
@@ -79,15 +111,47 @@ namespace Settings
         {
             auto& colors = settings["colors"];
 
-            /* Simple Colours  */
-            colors["background"] = ser_ImColor(ColourPresets.COLOUR_BACKGROUND);
-            colors["health"] = ser_ImColor(ColourPresets.COLOUR_HEALTH);
-            colors["health-downed"] = ser_ImColor(ColourPresets.COLOUR_HEALTH_DOWNED);
-            colors["health-defeated"] = ser_ImColor(ColourPresets.COLOUR_HEALTH_DEFEATED);
-            colors["health-shroud-necromancer"] = ser_ImColor(ColourPresets.COLOUR_SHROUD_NECROMANCER);
-            colors["health-shroud-specter"] = ser_ImColor(ColourPresets.COLOUR_SHROUD_SPECTER);
-            colors["barrier"] = ser_ImColor(ColourPresets.COLOUR_BARRIER);
-            colors["hovered"] = ser_ImColor(ColourPresets.COLOUR_HOVERED);
+            /* Default Colours  */
+            colors["default-background"] = ser_ImColor(ColourPresets.COLOUR_BACKGROUND);
+            colors["default-health"] = ser_ImColor(ColourPresets.COLOUR_HEALTH);
+            colors["default-health-downed"] = ser_ImColor(ColourPresets.COLOUR_HEALTH_DOWNED);
+            colors["default-health-defeated"] = ser_ImColor(ColourPresets.COLOUR_HEALTH_DEFEATED);
+            colors["default-health-shroud-necromancer"] = ser_ImColor(ColourPresets.COLOUR_SHROUD_NECROMANCER);
+            colors["default-health-shroud-specter"] = ser_ImColor(ColourPresets.COLOUR_SHROUD_SPECTER);
+            colors["default-barrier"] = ser_ImColor(ColourPresets.COLOUR_BARRIER);
+            colors["default-hovered"] = ser_ImColor(ColourPresets.COLOUR_HOVERED);
+            
+            /* Profession Colours */
+            colors["profession-background"] = ser_ImColor(ColourPresets.COLOUR_PROF_BACKGROUND);
+            colors["profession-health-elementalist"] = ser_ImColor(ColourPresets.COLOUR_PROF_HEALTH_ELEMENTALIST);
+            colors["profession-health-engineer"] = ser_ImColor(ColourPresets.COLOUR_PROF_HEALTH_ENGINEER);
+            colors["profession-health-guardian"] = ser_ImColor(ColourPresets.COLOUR_PROF_HEALTH_GUARDIAN);
+            colors["profession-health-mesmer"] = ser_ImColor(ColourPresets.COLOUR_PROF_HEALTH_MESMER);
+            colors["profession-health-necromancer"] = ser_ImColor(ColourPresets.COLOUR_PROF_HEALTH_NECROMANCER);
+            colors["profession-health-ranger"] = ser_ImColor(ColourPresets.COLOUR_PROF_HEALTH_RANGER);
+            colors["profession-health-revenant"] = ser_ImColor(ColourPresets.COLOUR_PROF_HEALTH_REVENANT);
+            colors["profession-health-thief"] = ser_ImColor(ColourPresets.COLOUR_PROF_HEALTH_THIEF);
+            colors["profession-health-warrior"] = ser_ImColor(ColourPresets.COLOUR_PROF_HEALTH_WARRIOR);
+            colors["profession-health-downed"] = ser_ImColor(ColourPresets.COLOUR_PROF_HEALTH_DOWNED);
+            colors["profession-health-defeated"] = ser_ImColor(ColourPresets.COLOUR_PROF_HEALTH_DEFEATED);
+            colors["profession-health-shroud-necromancer"] = ser_ImColor(ColourPresets.COLOUR_PROF_SHROUD_NECROMANCER);
+            colors["profession-health-shroud-specter"] = ser_ImColor(ColourPresets.COLOUR_PROF_SHROUD_SPECTER);
+            colors["profession-barrier"] = ser_ImColor(ColourPresets.COLOUR_PROF_BARRIER);
+            colors["profession-hovered"] = ser_ImColor(ColourPresets.COLOUR_PROF_HOVERED);
+            
+            /* Heat Map Colours */
+            colors["heat-map-background"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_BACKGROUND);
+            colors["heat-map-health-100"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_HEALTH_100);
+            colors["heat-map-health-75"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_HEALTH_75);
+            colors["heat-map-health-50"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_HEALTH_50);
+            colors["heat-map-health-25"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_HEALTH_25);
+            colors["heat-map-health-0"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_HEALTH_0);
+            colors["heat-map-health-downed"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_HEALTH_DOWNED);
+            colors["heat-map-health-defeated"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_HEALTH_DEFEATED);
+            colors["heat-map-health-shroud-necromancer"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_SHROUD_NECROMANCER);
+            colors["heat-map-health-shroud-specter"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_SHROUD_SPECTER);
+            colors["heat-map-barrier"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_BARRIER);
+            colors["heat-map-hovered"] = ser_ImColor(ColourPresets.COLOUR_HEATMAP_HOVERED);
             
             /** TODO: Move into separate `Border` config. */
             colors["border"] = ser_ImColor(BorderPresets.COLOUR_BORDER);
